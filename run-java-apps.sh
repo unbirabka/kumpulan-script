db3ec0689b1243c610fe6e7bc936f249ada5e4d0
akbaribnu <mr.akbaribnu@gmail.com>
2018-07-02 11:49:46 +0700

    update

--- run-java-apps.sh
+++ run-java-apps.sh
@@ -6,8 +6,6 @@ APP_NAME="awantunai-auth-service"
 APP_JAR="$WORKING_DIR/$APP_NAME/$APP"
 LOG_DIR="/mnt/nfs/logs/auth-service"
 
-echo "APP_JAR : $APP_JAR"
-
 JVM_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
 JVM_ARGS="-javaagent:$WORKING_DIR/newrelic/newrelic.jar -Dlogback.configurationFile=$WORKING_DIR/$APP_NAME/config/logback.xml -DconfigPath=$WORKING_DIR/$APP_NAME -server -Xms1g -Xmx1g -XX:MaxMetaspaceSize=256m -Xmn256m -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -XX:+PrintGCDateStamps -verbose:gc -XX:+PrintGCDetails -Xloggc:"$LOG_DIR/auth-service-gc" -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M -Dsun.net.inetaddr.ttl=60 -XX:+HeapDumpOnOutOfMemoryError"
 JMX_ARGS=" -Dcom.sun.management.jmxremote.port=19999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
